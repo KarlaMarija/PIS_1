4ecd8442f617164e1e29fc2c508126d7843975ee
KarlaMarija <kmarun00@fesb.hr>
2022-06-09 17:57:22 +0200

    Nova klasa

---
+++ PIS_1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIS_1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nova klasa");
+        }
+    }
+}
